4e75d12e5e19e112d4cfdf138ea565b3b4797d51
Greensue <1719837353@qq.com>
2017-08-24 17:53:41 +0800

    popup box when new a snapshot

--- Editor/EditorTest.cs
+++ Editor/EditorTest.cs
@@ -1,40 +1,105 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEditor;
+﻿
+
+ using UnityEditor;
+ using UnityEngine;
+ using System.Collections.Generic;
+
+ 
+ public class MyWindow : EditorWindow
+ {
 
 
-public class EditorTest : EditorWindow {
+ 	static Dictionary<string,string> snapTest = new Dictionary<string,string>();
+     bool groupEnabled;
+     bool myBool = true;
+     float myFloat = 1.23f;
+     static string SanpName;
+     
+     // Add menu item named "My Window" to the Window menu
+     [MenuItem("Window/My Window")]
+     public static void ShowWindow()
+     {
+         //Show existing window instance. If one doesn't exist, make one.
+        MyWindow window = (MyWindow)EditorWindow.GetWindow(typeof(MyWindow));
+        window.Show();
+     }
+     
+     void OnGUI()
+     {
+         
+         
+         groupEnabled = EditorGUILayout.BeginToggleGroup ("Optional Settings", groupEnabled);
+             myBool = EditorGUILayout.Toggle ("Toggle", myBool);
+             myFloat = EditorGUILayout.Slider ("Slider", myFloat, -3, 3);
+         EditorGUILayout.EndToggleGroup ();
+         if(GUILayout.Button("new napshot",GUILayout.MaxWidth(100)))
+         {
+         	DialogWindow window = (DialogWindow)EditorWindow.GetWindow(typeof(DialogWindow));
+        	window.Show();
+        	
 
-	// Use this for initialization
-	void Start () {
-		Debug.Log("Start");
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Debug.Log("Update");
-	
-	}
+         }
+         if(GUILayout.Button("Remove",GUILayout.MaxWidth(100)))
+         {
 
-	void Awake()
-	{
-		Debug.Log("Awake");
-	}
+         }
+         GUILayout.BeginHorizontal ("Toolbar");
+         foreach(string st in snapTest.Keys)
+         {
+         	if(GUILayout.Button(st,EditorStyles.toolbarButton,GUILayout.MaxWidth(100)))
+	        {
 
+	        }
+         }
+         GUILayout.EndHorizontal ();
+         
+         
+         
+         
+     }
 
-	[MenuItem("Window/EditorTest")]
-	public static void Init()
-	{
-		EditorTest window = (EditorTest)EditorWindow.GetWindow(typeof(EditorTest));
+     public class DialogWindow : EditorWindow
+	 {
+	    
+	    
+	     public static void ShowWindow()
+	     {
+	         //Show existing window instance. If one doesn't exist, make one.
+	        DialogWindow window = (DialogWindow)EditorWindow.GetWindow(typeof(DialogWindow));
+	        window.ShowUtility();
+	     }
 
-		window.Show();
-		Debug.Log("MenuItem window");
-	}
+	     public string myString;
+	     
+	     void OnGUI()
+	     {
+	         
+	         myString = EditorGUILayout.TextField ("Text Field", myString);
+	         if(GUILayout.Button("OK",GUILayout.MaxWidth(100))|| Input.GetKeyDown("enter"))
+	         {
+	         	SanpName = myString;
+	         	if(snapTest!=null && SanpName!= null)
+	         	{
+ 			        if(!snapTest.ContainsKey(SanpName))
+	         		snapTest.Add(SanpName,SanpName);
+	         		else
+	         		{//string text = string.Format("object '{0}' selected. (col={1})", foo.Name, col);
+        //Debug.Log(text);
+        				ShowNotification(new GUIContent(SanpName+" is already exist, please try another name!"));
+	         			Debug.LogWarningFormat(" '{0}' is already exist, please try another name!", SanpName);
+	         		}
 
+	         	}
+	         	else
+	         	{
+	         		Debug.LogWarningFormat("the snapshot name could not be empty!");
+	         		ShowNotification(new GUIContent("the snapshot name could not be empty!"));
+	         	}
+	         	Close();
 
-	public void OnGUI()
-	{
-		Debug.Log("OnGUI");
-	}
-}
+	         }
+	         
+	         
+	     }
+	 }
+ }
